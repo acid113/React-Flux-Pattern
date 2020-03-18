45f488fc46034eec2bbb6e8c299749508038dcb1
Marvin Estacio <marvin.estacio@willistowerswatson.com>
2020-03-18 20:09:00 +0800

    initial eslint setup

--- src/components/ColorComponent.jsx
+++ src/components/ColorComponent.jsx
@@ -1,36 +1,36 @@
 import React, {useState, useEffect} from "react";
 import ColorAppStore from "../stores/ColorAppStore";
 
-const ColorComponent  = () => {
-    const [color, setColor] = useState(ColorAppStore.getActiveColor());
+const ColorComponent = () => {
+	const [color, setColor] = useState(ColorAppStore.getActiveColor());
 
-    const onUpdateColor = () => {
-        console.log("inside ColorComponent.onUpdateColor()");
-        setColor(ColorAppStore.getActiveColor());
-    }
+	const onUpdateColor = () => {
+		console.log("inside ColorComponent.onUpdateColor()");
+		setColor(ColorAppStore.getActiveColor());
+	};
 
-    const onResetColor = () => {
-        console.log("inside ColorComponent.onResetColor()");
-        const tempColor = ColorAppStore.getActiveColor();
-        setColor(tempColor);
-    }
+	const onResetColor = () => {
+		console.log("inside ColorComponent.onResetColor()");
+		const tempColor = ColorAppStore.getActiveColor();
+		setColor(tempColor);
+	};
 
-    useEffect(() => {
-        console.log("inside ColorComponent().useEffect(), setting event listeners");
+	useEffect(() => {
+		console.log("inside ColorComponent().useEffect(), setting event listeners");
 
-        ColorAppStore.addChangeListener(onUpdateColor);
-        ColorAppStore.addResetChangeListener(onResetColor);
-        return () => {
-            ColorAppStore.removeChangeListener(onUpdateColor);
-            ColorAppStore.removeResetChangeListener(onResetColor);
-        };
-    }, []);
+		ColorAppStore.addChangeListener(onUpdateColor);
+		ColorAppStore.addResetChangeListener(onResetColor);
+		return () => {
+			ColorAppStore.removeChangeListener(onUpdateColor);
+			ColorAppStore.removeResetChangeListener(onResetColor);
+		};
+	}, []);
 
-    return (
-        <>
-            <div className="color-container" style={{backgroundColor: color}}></div>
-        </>
-    );
-}
+	return (
+		<>
+			<div className="color-container" style={{backgroundColor: color}}></div>
+		</>
+	);
+};
 
 export default ColorComponent;
